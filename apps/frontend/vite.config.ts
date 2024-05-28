6fc1baca3903ef0d4cf9bdd32939dde0d5e5c856
Benjamin Chiang <chiangbenjamin991@gmail.com>
2024-05-28 11:37:18 -0700

    feat(frontend/ts): add path alias to src

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { join } from 'path';
+import { join, resolve } from 'node:path';
 
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
@@ -12,6 +12,11 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     envDir,
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    },
     server: {
       port: PORT,
       proxy: {
